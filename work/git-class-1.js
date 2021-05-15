8af13752142ad5342e5d0da8f82584a8f72b9f29
tadesse123 <birbirsatadesse@gmail.com>
2021-05-15 09:35:40 -0700

    update my work

--- work/git-class-1.js
+++ work/git-class-1.js
@@ -4,12 +4,12 @@
  */
 
  // Tadesse - create a variable with a number between 0 - 100
- Tadesse = 10 , 42 , 67 ,87;
+  var tadesse = 10;
 
  // Uma - create a function that takes in variable from Tadesse, and return num + 1
 
  // Tsion - create a variable with the value as Tsion's favorite food
 
- // Kelvin - create a function that takes in Tsion's fav food, and return "I love " + tsion's fav food
 
+ // Kelvin - create a function that takes in Tsion's fav food, and return "I love " + tsion's fav food
  // Beth - Create a variable array, with everyone's name in it.
