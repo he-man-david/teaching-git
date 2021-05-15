c78acbe14fc09b75c1fbc390cd50abafb85505b6
david he man <david.he.is.here@gmail.com>
2021-05-15 08:51:56 -0700

    creating work for class

---
+++ work/git-class-1.js
@@ -0,0 +1,14 @@
+/**
+ * Today we will use this to collab on GIT
+ * 
+ */
+
+ // Tadesse - create a variable with a number between 0 - 100
+
+ // Uma - create a function that takes in variable from Tadesse, and return num + 1
+
+ // Tsion - create a variable with the value as Tsion's favorite food
+
+ // Kelvin - create a function that takes in Tsion's fav food, and return "I love " + tsion's fav food
+
+ // Beth - Create a variable array, with everyone's name in it.
